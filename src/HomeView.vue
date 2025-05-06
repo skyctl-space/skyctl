0d5a300a99eb6c9669c56f37dff610a5db3e6807
Diego Dompe <ddompe@gmail.com>
2025-05-06 08:51:35 -0600

    Fixes for MacOS framework packaging
    Signed-off-by: Diego Dompe <ddompe@gmail.com>

--- src/HomeView.vue
+++ src/HomeView.vue
@@ -18,7 +18,6 @@ const time = ref(0)
         <v-divider/>
         <v-card-text>
             Welcome to SkyCtl, your all-in-one astronomy control center.
-            <a href="/assets/js/stellarium-web-engine.wasm"> link</a>
         </v-card-text>
         <v-divider/>
         <v-card class="mx-auto" max-width="368">
